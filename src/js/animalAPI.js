21db35502225759c628ff20cbfe7445373111617
Jennifer von Trotta-Treyden <jv222th@student.lnu.se>
2024-09-02 22:32:08 +0200

    Finish the HTML for the response view, insert the API keys and endpoints and start binding the DOM elements in the submit handler function.

--- src/js/animalAPI.js
+++ src/js/animalAPI.js
@@ -1,7 +1,7 @@
 /**
  * A class that makes the handling of both the cat and dog API easier.
  *
- * Dog API - https://dog.ceo/dog-api/
+ * Dog API - https://thedogapi.com/
  * Cat API - https://thecatapi.com/
  *
  * @author Jennifer von Trotta-Treyden <jv222th>
@@ -11,5 +11,23 @@
  * A class to easier make requests to two animal APIs.
  */
 export class AnimalAPI {
+  /**
+   * The private API key for the cat API.
+   */
+  #catApiKey
 
+  /**
+   * The private API key for the dog API
+   */
+  #dogApiKey
+
+  /**
+   * Initiates the class.
+   */
+  constructor () {
+    this.#catApiKey = 'live_NW9uwsi4WpXH8w73tE9G66myOuOKtYbwyucbrXQAasYGA482YSmoVa3U0kEvzatS'
+    this.#dogApiKey = 'live_8j4ldt618sDExuPdSlKC4ocQVUkhLgLm5D4jJtiGvLIIigLW4yD8stOm5680ykJx'
+    this.DOGAPI = 'https://api.thedogapi.com/v1/images/'
+    this.CATAPI = 'https://api.thecatapi.com/v1/images/'
+  }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,23 @@
  * @author Jennifer von Trotta-Treyden <jv222th>
  */
 
+import { AnimalAPI } from './animalAPI'
+
 // Catch the submit event, stop the default behaviour and handle the input.
 const inputForm = document.querySelector('#inputForm')
+
 inputForm.addEventListener('submit', (event) => {
   event.preventDefault()
+  submitHandler()
 })
+
+/**
+ * Handles the form submission and manipulates the DOM.
+ */
+function submitHandler () {
+  const name = document.querySelector('#nameInput').value
+  const cat = document.querySelector('#radioAnswers [value="cat"]').checked
+  const animal = cat === true ? 'cat' : 'dog'
+
+  console.log(animal)
+}
